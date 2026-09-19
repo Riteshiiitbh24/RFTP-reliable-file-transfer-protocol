6c6132987cf3a035e0efd5ffb9c569c5d00bc4d4
ritesh1857 <kumarn68510@gmail.com>
2026-09-20 02:47:39 +0530

    test: phase 4 - simulate network packet loss

--- server.py
+++ server.py
@@ -1,21 +1,31 @@
 import socket
 import struct
+import random
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8080
 HEADER_FORMAT = '!IBHH'
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+DROP_PROBABILITY = 0.3  # 30% chance to drop a packet to simulate network loss
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((SERVER_IP, SERVER_PORT))
 
 print(f"RFTP Server listening on {SERVER_IP}:{SERVER_PORT}...")
+print(f"Network simulation active: {DROP_PROBABILITY * 100}% packet drop rate")
 
 def receive_file(output_filename):
     expected_seq = 0
     with open(output_filename, 'wb') as f:
         while True:
             packet, addr = server_socket.recvfrom(1024 + HEADER_SIZE)
+            
+            # --- PHASE 4: SIMULATED PACKET LOSS ---
+            if random.random() < DROP_PROBABILITY:
+                print("SIMULATED DROP: Packet lost in transit!")
+                continue  # Skips the rest of the loop. No ACK is sent.
+            # --------------------------------------
+
             header = packet[:HEADER_SIZE]
             seq_num, packet_type, length, checksum = struct.unpack(HEADER_FORMAT, header)
             
@@ -28,13 +38,11 @@ def receive_file(output_filename):
                 
             # Type 0 = DATA
             elif packet_type == 0:
-                # Only write to file if it's the exact packet we are waiting for
                 if seq_num == expected_seq:
                     data = packet[HEADER_SIZE:HEADER_SIZE+length]
                     f.write(data)
                     expected_seq += 1
                 
-                # Send ACK (Type 1) back to the client
                 ack_header = struct.pack(HEADER_FORMAT, seq_num, 1, 0, 0)
                 server_socket.sendto(ack_header, addr)
 
